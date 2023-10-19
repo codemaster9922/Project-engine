644e41590fb593f695665f750c87b2e552beb38f
codemaster9922 <148446568+codemaster9922@users.noreply.github.com>
2023-10-19 21:19:39 +0530

    Add files via upload

---
+++ CHESS_BOARD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "R N B Q K B N R" << endl;
+	cout << "P P P P P P P P" << endl;
+	cout << "X X X X X X X X" << endl;
+	cout << "X X X X X X X X" << endl;
+	cout << "X X X X X X X X" << endl;
+	cout << "X X X X X X X X" << endl;
+	cout << "P P P P P P P P" << endl;
+	cout << "R N B Q K B N R" << endl;
+
+	return 0;
+}
