9a4c1b4b958d25bcd71208fdc3a763d9b0555e6b
codemaster9922 <148446568+codemaster9922@users.noreply.github.com>
2023-11-01 09:41:58 +0000

    include constexpr for piece name

--- chessboard.cpp
+++ chessboard.cpp
@@ -3,37 +3,50 @@
 using namespace std;
 
 int main()
-{
+{ constexpr char wKnight ='N';
+  constexpr char bKngiht ='n';
+  constexpr char wBishop ='B';
+  constexpr char bBishop ='b';
+  constexpr char wRook = 'R';
+  constexpr char bRook = 'r';
+  constexpr char wQueen= 'Q';
+  constexpr char bQueen= 'q';
+  constexpr char wKing= 'K';
+  constexpr char bKing= 'k';
+  constexpr char wPawn= 'P';
+  constexpr char bPawn= 'p';
+  constexpr char emptySquares='X';
+
 	
 	char chessboard[63];
-	chessboard[0] = 'R';
-	chessboard[7] = 'R';
-	chessboard[1] = 'N';
-	chessboard[6] = 'N';
-	chessboard[2] = 'B';
-	chessboard[5] = 'B';
-	chessboard[3] = 'Q';
-	chessboard[4] = 'K';
+	chessboard[0] = wRook;
+	chessboard[7] = wRook;
+	chessboard[1] = wKnight;
+	chessboard[6] = wKnight;
+	chessboard[2] = wBishop;
+	chessboard[5] = wBishop;
+	chessboard[3] = wQueen;
+	chessboard[4] = wKing;
 	for (int i = 8; i < 16; i++)
 	{
-		chessboard[i] = 'P';
+		chessboard[i] = wPawn;
 	}
 	for (int j = 16; j < 48; j++)
 	{
-		chessboard[j] = 'X';
+		chessboard[j] = emptySquares;
 	}
 	for (int k = 48; k < 56; k++)
 	{
-		chessboard[k] = 'p';
+		chessboard[k] = bPawn;
 	}
-	chessboard[56] = 'r';
-	chessboard[57] = 'n';
-	chessboard[58] = 'b';
-	chessboard[59] = 'q';
-	chessboard[60] = 'k';
-	chessboard[61] = 'b';
-	chessboard[62] = 'n';
-	chessboard[63] = 'r';
+	chessboard[56] = bRook;
+	chessboard[57] = bKngiht;
+	chessboard[58] = bBishop;
+	chessboard[59] = bQueen;
+	chessboard[60] = bKing;
+	chessboard[61] = bBishop;
+	chessboard[62] = bKngiht;
+	chessboard[63] = bRook;
 
 	for (int a = 56; a < 64; a++)
 	{
@@ -75,5 +88,7 @@ int main()
 		cout << chessboard[h] << " ";
 	}
 	cout << endl;
+
+	
 	return 0;
 }
